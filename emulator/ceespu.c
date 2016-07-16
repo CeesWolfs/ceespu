30795651c0268dfe26d4d42d0e7eac9d6e7e8043
ceeswolfs <ceeswolfs1985@gmail.com>
2016-07-16 22:10:09 +0200

    overhauled instruction set

--- emulator/ceespu.c
+++ emulator/ceespu.c
@@ -48,12 +48,20 @@ static inline unsigned char take_jump(unsigned char branch_condition, unsigned s
 }
 
 
-static inline short vmem_get_word(VirtualMem* vm,unsigned short address) {
+static inline unsigned short vmem_get_word(VirtualMem* vm,unsigned short address) {
 	return vm->mem[address >> 1]._words[address && 0x0001];
+}
+
+static inline unsigned char vmem_get_byte(VirtualMem* vm,unsigned short address) {
+	return vm->mem[address >> 2]._bytes[address && 0x0003];
 }
 
 static inline void vmem_store_word(VirtualMem* vm,unsigned short address,unsigned short value) {
 	vm->mem[address >> 1]._words[address && 0x0001] = value;
+}
+
+static inline void vmem_store_byte(VirtualMem* vm,unsigned short address,unsigned char value) {
+	vm->mem[address >> 2]._words[address && 0x0003] = value;
 }
 
 VirtualMachine* vmachine_create() {
@@ -71,7 +79,6 @@ void vmachine_dump(VirtualMachine* vm){
 	{
 		printf("register: c%d 0x%04x\n", i,vm->RegFile[i]);
 	}
-	printf("stack_pointer: 0x%04x\n", vm->stack_pointer);
 	printf("PC: 0x%04x\n", vm->PC);
 }
 
@@ -88,20 +95,19 @@ void vmachine_run(VirtualMachine* vm) {
 		instruction = vm->vmem->mem[vm->PC++]._dword;
 		cat = instruction & 0x03;
 		op = (instruction  >> 2 ) & 0xf;
-		r1 = (instruction >> 6) & 0x1f;
-		r2 = (instruction >> 11) & 0x1f;
+		rd = (instruction >> 6) & 0x1f;
+		r1 = (instruction >> 11) & 0x1f;
+		r2 = (instruction >> 16) & 0x1f;
 		imm = instruction >> 16;
 		isaluop = !(cat == 3);
 		switch (cat) {
-		    case 0: vm->RegFile[imm & 0x1f] = run_alu_op(op, vm->RegFile[r1], vm->RegFile[r2], vm->carry_flag); break;
-			case 1: vm->RegFile[r2] = run_alu_op(op, vm->RegFile[r1], imm, vm->carry_flag); break;
-			case 2: vm->RegFile[r2] = run_alu_op(op, imm, vm->RegFile[r1], vm->carry_flag); break;
+		        case 0: vm->RegFile[rd] = run_alu_op(op, vm->RegFile[r1], vm->RegFile[r2], vm->carry_flag); break;
+			case 1: vm->RegFile[rd] = run_alu_op(op, vm->RegFile[r1], imm, vm->carry_flag); break;
+			case 2: vm->RegFile[rd] = run_alu_op(op, imm, vm->RegFile[r1], vm->carry_flag); break;
 			default: switch(op) {
-			case 0: if (imm && (1 << 15)) { vmem_store_word(vm->vmem, imm & 0x7fff, vm->RegFile[r1]); } else { vmem_store_word(vm->vmem, vm->RegFile[r2], vm->RegFile[r1]); } break;
+			        case 0: vmem_store_word(vm->vmem, imm & 0x7fff, vm->RegFile[r1]);  
 				case 1: if (imm && (1 << 15)) { vm->RegFile[r1] = vmem_get_word(vm->vmem, imm & 0x7fff); } else { vm->RegFile[r1] = vmem_get_word(vm->vmem, vm->RegFile[r2]); } break;
-				case 2: vmem_store_word(vm->vmem, vm->stack_pointer, vm->RegFile[r1]); vm->stack_pointer--; break;
-				case 3: vm->RegFile[r1] = vmem_get_word(vm->vmem, vm->stack_pointer); vm->stack_pointer++;   break;
-				case 4: vm->RegFile[18] = vm->PC; vm->PC = imm; break;
+				case 2: vm->RegFile[18] = vm->PC; vm->PC = imm; break;
 				case 8: if (take_jump(imm >> 14, vm->RegFile[r1], vm->RegFile[r2])) { vm->PC = imm & 0x3fff; continue; } break;
 				case 9: if (take_jump(imm >> 14 + 4, vm->RegFile[r1], vm->RegFile[r2])) { vm->PC = imm && 0x3fff; continue; } break;
 				case 10: goto stop;
