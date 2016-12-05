7d1499cf2ee506c56df68fd7185bf882a84f6107
Cees Wolfs <ceeslol123@gmail.com>
2016-12-05 21:50:35 +0100

    fix indentation

--- src/ceespu_alu.v
+++ src/ceespu_alu.v
@@ -19,46 +19,46 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ceespu_alu(
-    input I_clk,
-	input I_rst,
-    input [31:0] I_dataA,
-    input [31:0] I_dataB,
-    input I_Cin,
-	input [3:0] I_aluop,
-	output reg O_multiCycle,
-    output reg [31:0] O_dataResult,
-	output reg O_Cout,
-    output O_dataReady
-    );
+  input I_clk,
+  input I_rst,
+  input [31:0] I_dataA,
+  input [31:0] I_dataB,
+  input I_Cin,
+  input [3:0] I_aluop,
+  output reg O_multiCycle,
+  output reg [31:0] O_dataResult,
+  output reg O_Cout,
+  output O_dataReady
+);
 
-always @* begin
-	O_multiCycle 	<= 0;
+  always @* begin
+    O_multiCycle 	<= 0;
     case(I_aluop) 
       4'd1:
         begin
-		  O_dataResult <= I_dataA | I_dataB;
-		  O_Cout <= 0;
-		end
+          O_dataResult <= I_dataA | I_dataB;
+          O_Cout <= 0;
+        end
       4'd2:
         begin
-		  O_dataResult <= I_dataA & I_dataB;
-		  O_Cout <= 0;
-		end
+          O_dataResult <= I_dataA & I_dataB;
+          O_Cout <= 0;
+        end
       4'd3:
         begin
-		  O_dataResult <= I_dataA ^ I_dataB;
-		  O_Cout <= 0;
-		end
+          O_dataResult <= I_dataA ^ I_dataB;
+          O_Cout <= 0;
+        end
       4'd4:
         begin
-		  O_dataResult <= {{(24){I_dataA[7]}}, I_dataA[7:0]};
-		  O_Cout <= 0;
+          O_dataResult <= {{(24){I_dataA[7]}}, I_dataA[7:0]};
+          O_Cout <= 0;
         end
       4'd5:
         begin
-		  O_dataResult <= {{(16){I_dataA[15]}}, I_dataA[15:0]};
-		  O_Cout <= 0;
-		end
+          O_dataResult <= {{(16){I_dataA[15]}}, I_dataA[15:0]};
+          O_Cout <= 0;
+        end
       4'd6:
         begin 
           O_dataResult <= I_dataA << I_dataB[4:0];
@@ -85,37 +85,37 @@ always @* begin
           {O_Cout, O_dataResult} <= I_dataA + I_dataB + I_Cin;
         end
     endcase
-end
-  
-reg [1:0]   mul_counter = 0;
-reg [31:0]	mul_result;
-reg [31:0]	mul_tmp1;
-reg [31:0]	a_in;
-reg [31:0]	b_in;
-  
-assign O_dataReady = O_multiCycle & (mul_counter == 2);
-/* Synchronous logic */
-always @(posedge I_clk) begin
-	if(I_rst)
-	  begin
-	    a_in		<= 0;
-		b_in		<= 0;
-		mul_tmp1	<= 0;
-		mul_result  <= 0;
-		mul_counter <= 0;
+  end
+
+  reg [1:0]   mul_counter = 0;
+  reg [31:0]	mul_result;
+  reg [31:0]	mul_tmp1;
+  reg [31:0]	a_in;
+  reg [31:0]	b_in;
+
+  assign O_dataReady = O_multiCycle & (mul_counter == 2);
+  /* Synchronous logic */
+  always @(posedge I_clk) begin
+    if(I_rst)
+      begin
+        a_in		<= 0;
+        b_in		<= 0;
+        mul_tmp1	<= 0;
+        mul_result  <= 0;
+        mul_counter <= 0;
       end
-	else 
-	  begin	// infer pipelined multiplier
-		a_in		<= I_dataA;
-		b_in		<= I_dataB;
-		mul_tmp1	<= a_in * b_in;
-		mul_result	<= mul_tmp1;
-		if (mul_counter == 3 | O_dataReady)
-	      mul_counter 	<= 0;
-		else if(O_multiCycle)
-		  mul_counter 	<= mul_counter + 1;
-	  end
-end
+    else 
+      begin	// infer pipelined multiplier
+        a_in		<= I_dataA;
+        b_in		<= I_dataB;
+        mul_tmp1	<= a_in * b_in;
+        mul_result	<= mul_tmp1;
+        if (mul_counter == 3 | O_dataReady)
+          mul_counter 	<= 0;
+        else if(O_multiCycle)
+          mul_counter 	<= mul_counter + 1;
+      end
+  end
 
 
 endmodule
--- src/ceespu_compare.v
+++ src/ceespu_compare.v
@@ -19,24 +19,24 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ceespu_compare(
-    input [31:0] I_dataA,
-    input [31:0] I_dataB,
-    input [2:0] I_branchOp,
-    input I_Cin,
-    output reg O_doBranch
-    );
+  input [31:0] I_dataA,
+  input [31:0] I_dataB,
+  input [2:0] I_branchOp,
+  input I_Cin,
+  output reg O_doBranch
+);
 
-always @* begin
-   case (I_branchOp) 
-	 0: O_doBranch = (I_dataA == I_dataB);
-	 1: O_doBranch = (I_dataA != I_dataB);
-	 2: O_doBranch = (I_dataA > I_dataB);
-	 3: O_doBranch = (I_dataA >= I_dataB);
-	 4: O_doBranch = ($signed(I_dataA) > $signed(I_dataB));
-	 5: O_doBranch = ($signed(I_dataA) >= $signed(I_dataB));
-	 6: O_doBranch = I_Cin;
-	 7: O_doBranch = 1;
-   endcase
-end
+  always @* begin
+    case (I_branchOp) 
+      0: O_doBranch = (I_dataA == I_dataB);
+      1: O_doBranch = (I_dataA != I_dataB);
+      2: O_doBranch = (I_dataA > I_dataB);
+      3: O_doBranch = (I_dataA >= I_dataB);
+      4: O_doBranch = ($signed(I_dataA) > $signed(I_dataB));
+      5: O_doBranch = ($signed(I_dataA) >= $signed(I_dataB));
+      6: O_doBranch = I_Cin;
+      7: O_doBranch = 1;
+    endcase
+  end
 
 endmodule
