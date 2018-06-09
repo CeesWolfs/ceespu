b9ca8efdbec16d167e9d046e3dd5693db413000e
ceeswolfs <ceeswolfs1985@gmail.com>
2018-06-09 18:27:37 +0200

    Update crt0.s

--- code/crt0.s
+++ code/crt0.s
@@ -7,21 +7,26 @@ int_vector:
 
 	.globl	uart_rx_irq
 	.align	4
-timer_irq:                              ; @timer_irq
-	addi cir,cir,-4                       ; %entry
-	sw c1, -4(csp)
+timer_irq:                                ; @timer_irq
+	sw c1, -4(csp)                        ; @entry
 	lw c1, system_timer_irq_counter(c0)
-  addi c1, c1, 1
-  sw c1, system_timer_irq_counter(c0)
-  lw c1, -4(csp)
+ 	addi c1, c1, 1
+ 	sw c1, system_timer_irq_counter(c0)
+ 	lw c1, -4(csp)
 	bx cir
  
- .globl	uart_rx_irq
+	.globl	uart_rx_irq
 	.align	4
-uart_rx_irq:                            ; @uart_rx_irq 
-	addi cir,cir,-4                       ; %entry   
-	lbu c1, 65524(c0)
-	sb c1, 65528(c0)
+uart_rx_irq:                               ; @uart_rx_irq 
+	sw c1, -8(csp)                         ; %entry
+	sw c2, -4(csp)
+	lw c2, 65532
+	lw c1, system_receive_irq_buffer_size(c0)
+	sw c2, system_receive_irq_buffer(c1)
+ 	addi c1, c1, 1
+ 	sw c1, system_receive_irq_buffer_size(c0)
+ 	lw c1, -4(csp)
+	lw c2, -8(csp)
 	bx cir
   
   .data
