debf33e460c8c464be50a2d9e3196f08f095e651
ceeswolfs <ceeswolfs1985@gmail.com>
2018-06-09 18:16:04 +0200

    Create crt0.s

---
+++ code/crt0.s
@@ -0,0 +1,30 @@
+; The ceespu common runtime which sets up the runtime enviroment and interrupt handles
+; This file is linked into every ceespu executable.
+int_vector:
+	b main
+	b uart_rx_irq
+	b timer_irq
+
+	.globl	uart_rx_irq
+	.align	4
+timer_irq:                              ; @timer_irq
+	addi cir,cir,-4                       ; %entry
+	sw c1, -4(csp)
+	lw c1, system_timer_irq_counter(c0)
+  addi c1, c1, 1
+  sw c1, system_timer_irq_counter(c0)
+  lw c1, -4(csp)
+	bx cir
+ 
+ .globl	uart_rx_irq
+	.align	4
+uart_rx_irq:                            ; @uart_rx_irq 
+	addi cir,cir,-4                       ; %entry   
+	lbu c1, 65524(c0)
+	sb c1, 65528(c0)
+	bx cir
+  
+  .data
+  .comm	system_timer_irq_counter,4,4
+  .comm system_receive_irq_buffer,59,1
+  .comm system_receive_irq_buffer_size,1,1
